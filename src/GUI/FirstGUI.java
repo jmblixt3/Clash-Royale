60bb890df1ac428ee6be133e379d965c5bb0a3da
Jonathan Blixt <jmblixt3@gmail.com>
2017-09-03 14:45:04 -0500

    stuff

---
+++ src/GUI/FirstGUI.java
@@ -0,0 +1,10 @@
+package GUI;
+
+import java.io.IOException;
+
+public class FirstGUI
+{
+	public static void main(String[] args) throws InterruptedException, IOException{
+		
+	}
+}
---
+++ src/application/Main.java
@@ -0,0 +1,83 @@
+package application;
+
+import static javafx.geometry.HPos.RIGHT;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.*;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+
+public class Main extends Application
+{
+	@Override
+	public void start(Stage primaryStage)
+	{  primaryStage.setTitle("JavaFX Welcome");
+    GridPane grid = new GridPane();
+    grid.setAlignment(Pos.CENTER);
+    grid.setHgap(10);
+    grid.setVgap(10);
+    grid.setPadding(new Insets(25, 25, 25, 25));
+
+    Text scenetitle = new Text("Welcome");
+    scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+    grid.add(scenetitle, 0, 0, 2, 1);
+
+    Label userName = new Label("User Name:");
+    grid.add(userName, 0, 1);
+
+    TextField userTextField = new TextField();
+    grid.add(userTextField, 1, 1);
+
+    Label pw = new Label("Password:");
+    grid.add(pw, 0, 2);
+
+    PasswordField pwBox = new PasswordField();
+    grid.add(pwBox, 1, 2);
+
+    Button btn = new Button("Sign in");
+    HBox hbBtn = new HBox(10);
+    hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+    hbBtn.getChildren().add(btn);
+    grid.add(hbBtn, 1, 4);
+
+    final Text actiontarget = new Text();
+    grid.add(actiontarget, 0, 6);
+    grid.setColumnSpan(actiontarget, 2);
+    grid.setHalignment(actiontarget, RIGHT);
+    actiontarget.setId("actiontarget");
+
+    btn.setOnAction(new EventHandler<ActionEvent>() {
+
+        @Override
+        public void handle(ActionEvent e) {
+            actiontarget.setFill(Color.FIREBRICK);
+            actiontarget.setText("Sign in button pressed");
+        }
+    });
+    Scene scene = new Scene(grid, 300, 275);
+    primaryStage.setScene(scene);
+    scene.getStylesheets().add
+     (Main.class.getResource("application.css").toExternalForm());
+    primaryStage.show();
+	}
+
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
+}
