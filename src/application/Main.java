45b934e8830531eb2f78a6bd2273be06b0ea25a0
Jonathan Blixt <jmblixt3@gmail.com>
2017-09-03 16:05:23 -0500

    i ha7te commit messages and I should probably use them

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,6 @@
 package application;
 
-import static javafx.geometry.HPos.RIGHT;
+import static javafx.geometry.HPos.*;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
@@ -11,12 +11,9 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
